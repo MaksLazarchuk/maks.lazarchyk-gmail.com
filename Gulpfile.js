c2e253d9b84c878a3d595de1a8cdf6318b7fea2b
MaksLazarchuk <maks.lazarchyk@gmail.com>
2020-04-17 12:25:51 +0300

    task9: gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
- 
+
 gulp.task('sass', function () {
   return gulp.src('./assets/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./dist'));
 });
- 
+
 gulp.task('sass:watch', function () {
-    return gulp.watch('./assets/**/*.scss',gulp.series('sass'));
+  return gulp.watch('./assets/**/*.scss', gulp.series('sass'));
 });
