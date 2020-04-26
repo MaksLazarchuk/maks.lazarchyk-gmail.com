33835d20348935e70c4602f588913ed8ce59476e
MaksLazarchuk <maks.lazarchyk@gmail.com>
2020-04-26 22:09:09 +0300

    server done

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const path = require('path');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../pages/index.html'));
+    res.sendFile(path.resolve(__dirname, './pages/index.html'));
 });
 
 app.listen(port, function () {
