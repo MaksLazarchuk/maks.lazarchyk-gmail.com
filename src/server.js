f5376d2284b5163cbae1ccf59a27ed5d5802b434
NazarLazarchuk <nazik.lazarchyk@gmail.com>
2020-04-27 12:53:59 +0300

    Form submit, Post request on back-end

--- src/server.js
+++ src/server.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 const port = 80;
 
 const path = require('path');
 
 app.use(express.static('public'));
+app.use(bodyParser());
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './pages/index.html'));
+});
+
+app.post('/form', (req, res) => {
+    const { name, email, message } = req.body;
+    const text = `${name} - ${email} - ${message}`;
+
+    fs.writeFile('form.txt', text, (err) => {
+        if (err) throw err;
+        console.log('Файл успішно створений');
+    });
+    
+    res.send('Відгук надісланий!');
 });
 
 app.listen(port, function () {
